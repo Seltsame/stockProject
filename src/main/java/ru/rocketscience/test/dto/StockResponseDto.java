3c0f08f2f984c4038622875ad6c9fede06d1cbc9
Seltsame <spirit02@ya.ru>
2020-12-10 18:23:28 +0700

    Добавлен MapStruct

--- src/main/java/ru/rocketscience/test/dto/StockResponseDto.java
+++ src/main/java/ru/rocketscience/test/dto/StockResponseDto.java
@@ -3,7 +3,6 @@ package ru.rocketscience.test.dto;
 import lombok.*;
 
 @Value
-@Builder
 public class StockResponseDto {
 
     String name;
--- src/main/java/ru/rocketscience/test/service/StockService.java
+++ src/main/java/ru/rocketscience/test/service/StockService.java
@@ -3,6 +3,7 @@ package ru.rocketscience.test.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.rocketscience.test.dto.StockResponseDto;
+import ru.rocketscience.test.mapper.StockMapper;
 import ru.rocketscience.test.model.Stock;
 import ru.rocketscience.test.repository.StockRepository;
 
@@ -13,19 +14,14 @@ import java.util.Optional;
 public class StockService {
 
     private final StockRepository stockRepository;
+    private final StockMapper stockMapper;
 
     public StockResponseDto getById(Long id) {
         Optional<Stock> optStock = stockRepository.findById(id);
         if (!optStock.isPresent()) {
             return null;
         }
-        return getStockResponseDtoFromStock(optStock.get());
+        return stockMapper.fromEntity(optStock.get());
     }
 
-    public static StockResponseDto getStockResponseDtoFromStock(Stock stock) {
-        return StockResponseDto.builder()
-                .name(stock.getName())
-                .city(stock.getCity())
-                .build();
-    }
 }
